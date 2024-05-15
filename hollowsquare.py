c015f1e0c2669fec9dfcbf140bae01d53e1c71f2
Ayan <ayanalyser@gmail.com>
2024-05-15 16:02:34 +0530

    Create hollowsquare.py

---
+++ hollowsquare.py
@@ -0,0 +1,20 @@
+Target Pattern:
+*****
+
+*   *
+
+*   *
+
+*   *
+
+*****
+
+Code:
+row = int(input("Enter number of rows:"))
+for i in range (1,row+1):
+    for j in range (1,row+1):
+        if i==1 or i==row or j==1 or j==row:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print("\n")
